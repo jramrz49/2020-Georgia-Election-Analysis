148cdcb0d76d288199c5f81d28eb50fabb1f68bd
jramrz49 <68242956+jramrz49@users.noreply.github.com>
2020-11-22 22:09:01 -0600

    Add files via upload

---
+++ Georgiamap.R
@@ -0,0 +1,32 @@
+library(sf)
+library(ggplot2)
+library(tmap)
+library(tmaptools)
+library(leaflet)
+library(dplyr)
+library(readxl)
+options(scipen = 999)
+?options
+
+setwd('V:/Stat382') # Use your own
+
+data <- read_excel('Cleaned_Data.xlsx')
+mymap <- st_read("Counties_Georgia.shp", stringsAsFactors=F)
+str(mymap)
+
+
+colnames(mymap)[colnames(mymap) %in% c('NAME10')] <- 'County'
+map_and_data <- inner_join(mymap,data)
+
+#?colnames
+#?inner_join
+
+#ggplot(map_and_data)+
+  #geom_sf(aes(fill= Education))
+
+ggplot(map_and_data)+
+  geom_sf(aes(fill=Education))+
+  scale_fill_gradient(low= '#56B1F7',high='#132B43')
+  
+
+
